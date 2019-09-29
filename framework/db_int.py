d2b0c5a4f86b4981a1a33a882b54234aa60ece46
Sanjay Risbud <sanjayrisbud@gmail.com>
2019-09-29 18:01:08 +0800

    interface, for all db interfaces

---
+++ framework/db_int.py
@@ -0,0 +1,35 @@
+# An interface which must be implemented by all classes that act as database interfaces
+
+class DatabaseInterface:
+    
+    _conn = None
+    _logger = None
+
+    def __init__(self, logger):
+        if logger:
+            self.set_logger(logger)
+
+    def set_logger(self, logger):
+        self._logger = logger
+
+    def execute(self, script, params=None, return_rs=False):
+        raise NotImplementedError
+
+    def query(self, script, params=None):
+        raise NotImplementedError
+
+    def find(self, dbtype):
+        raise NotImplementedError
+
+    def delete(self, dbtype):
+        raise NotImplementedError
+
+    def store(self, dbtype):
+        raise NotImplementedError
+
+    def create(self, dbtype):
+        raise NotImplementedError
+
+    def close(self):
+        self._conn.close()
+        self._logger = None
