dc8496fc97a0330cae3463eba30e1e6ed975f893
Sanjay Risbud <sanjayrisbud@gmail.com>
2019-10-02 00:33:42 +0800

    test db functionality

--- test/test_robot_2.py
+++ test/test_robot_2.py
@@ -21,13 +21,22 @@ class TestRobot(RawRequests):
         self.tt.Symbol = "ALI"
         self.tt.Description = "Ayala Land Inc"
         self.tt.Price = 51.75
-        self._database.store(self.tt)
+        
+        self._database.store(self.tt)   # insert record
+
+        self.tt.Price = 63
+        self._database.store(self.tt)   # update record
 
         self.tt.StockId = 76
         self.tt.Symbol = "JFC"
         self.tt.Description = "Jollibee Foods Corporation"
         self.tt.Price = 228.0
+        self.logger.debug(self._database.find(self.tt))    # False
+        self._database.delete(self.tt)  # nothing deleted
 
+        self._database.store(self.tt)   # insert record
+        self.logger.debug(self._database.find(self.tt))    # True
+        self._database.delete(self.tt)  # 1 record deleted
 
 if __name__ == "__main__":
     tr = TestRobot(sys.argv)
